a6ddf5a6009127988ea1bc85f6124b09af1dd7fa
o-tao <dhtkdals369@gmail.com>
2025-04-26 23:20:52 +0900

    refactor: 이미지 업로드 로직 리팩토링, 동작흐름에 따른 step 주석 추가

--- src/main/java/example/image/service/ImageService.java
+++ src/main/java/example/image/service/ImageService.java
@@ -39,43 +39,48 @@ public class ImageService {
     @Value("${aws.s3.bucket-name}")
     private String bucketName;
 
-    // [public 메서드] 외부에서 사용, DB에 저장된 이미지의 uploadToken을 반환
+    /**
+     * [public 메서드]
+     * - 외부에서 사용, DB에 저장된 이미지의 uploadToken을 반환
+     */
     public String upload(ImageUploadInfo imageUploadInfo) {
+        // [Step 1] 요청값에 uploadToken 이 존재하지 않을 경우 uploadToken 생성
         String uploadToken = (imageUploadInfo.getUploadToken() == null || imageUploadInfo.getUploadToken().isBlank())
                 ? UUID.randomUUID().toString()
                 : imageUploadInfo.getUploadToken();
 
-        // 각 이미지를 S3에 업로드
-        List<String> urls = imageUploadInfo.getImages().stream()
-                .map(this::uploadImage)
+        // [Step 2] 모든 파일 유효성 검사
+        imageUploadInfo.getImages().forEach(file -> validateFile(file.getOriginalFilename()));
+
+        // [Step 3] 모든 파일 검증 완료 후 업로드
+        List<String> imageUrls = imageUploadInfo.getImages().stream()
+                .map(this::uploadImageToS3)
                 .toList();
 
-        // 업로드된 이미지를 저장
-        createImage(urls, uploadToken);
+        // [Step 4] 업로드 된 파일 DB 저장
+        createImage(imageUrls, uploadToken);
 
+        // [Step 5] uploadToken 반환
         return uploadToken;
     }
 
-    // [private 메서드] validateFile메서드를 호출하여 유효성 검증 후 uploadImageToS3메서드에 데이터를 반환하여 S3에 파일 업로드, public url을 받아 uploadImageToS3 서비스 로직에 반환
-    private String uploadImage(MultipartFile file) {
-        validateFile(file.getOriginalFilename()); // 파일 유효성 검증
-        return uploadImageToS3(file); // 이미지를 S3에 업로드하고, 저장된 파일의 public url을 uploadImageToS3 서비스 로직에 반환
-    }
-
-    // [private 메서드] 파일 유효성 검증
+    /**
+     * [private 메서드]
+     * - 파일 유효성 검증
+     */
     private void validateFile(String filename) {
-        // 파일 존재 유무 검증
+        // [Step 2-1] 파일 존재 유무 검증
         if (filename == null || filename.isEmpty()) {
             throw new CustomApplicationException(ErrorCode.NOT_EXIST_FILE);
         }
 
-        // 확장자 존재 유무 검증
+        // [Step 2-2] 확장자 존재 유무 검증
         int lastDotIndex = filename.lastIndexOf(".");
         if (lastDotIndex == -1) {
             throw new CustomApplicationException(ErrorCode.NOT_EXIST_FILE_EXTENSION);
         }
 
-        // 허용되지 않는 확장자 검증
+        // [Step 2-3] 허용되지 않는 확장자 검증
         String extension = URLConnection.guessContentTypeFromName(filename);
         List<String> allowedExtentionList = Arrays.asList("image/jpg", "image/jpeg", "image/png", "image/gif");
         if (extension == null || !allowedExtentionList.contains(extension)) {
@@ -83,42 +88,50 @@ public class ImageService {
         }
     }
 
-    // [private 메서드] 직접적으로 S3에 업로드
-    private String uploadImageToS3(MultipartFile file) {
-        // 원본 파일 명
-        String originalFilename = file.getOriginalFilename();
-        // 확장자 명
-        String extension = Objects.requireNonNull(originalFilename).substring(originalFilename.lastIndexOf(".") + 1);
-        // 변경된 파일
-        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + "_" + originalFilename;
+    /**
+     * [private 메서드]
+     * - S3 업로드
+     */
+    private String uploadImageToS3(MultipartFile image) {
+        String originalFilename = image.getOriginalFilename(); // 원본 파일 명
+        String extension = Objects.requireNonNull(originalFilename)
+                .substring(originalFilename.lastIndexOf(".") + 1); // 확장자 명
+        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + "_" + originalFilename; // 변경된 파일
 
-        // 이미지 파일 -> InputStream 변환
-        try (InputStream inputStream = file.getInputStream()) {
+        // [Step 3-1] 이미지 파일 -> InputStream 변환
+        try (InputStream inputStream = image.getInputStream()) {
             // PutObjectRequest 객체 생성
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucketName) // 버킷 이름
                     .key(s3FileName) // 저장할 파일 이름
                     .acl(ObjectCannedACL.PUBLIC_READ) // 퍼블릭 읽기 권한
                     .contentType("image/" + extension) // 이미지 MIME 타입
-                    .contentLength(file.getSize()) // 파일 크기
+                    .contentLength(image.getSize()) // 파일 크기
                     .build();
-            // S3에 이미지 업로드
-            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
+
+            // [Step 3-2] S3에 이미지 업로드
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, image.getSize()));
+
         } catch (Exception exception) {
             log.error(exception.getMessage(), exception);
             throw new CustomApplicationException(ErrorCode.IO_EXCEPTION_UPLOAD_FILE);
         }
 
-        // public url 반환
+        // [Step 3-3] public url 반환
         return s3Client.utilities().getUrl(url -> url.bucket(bucketName).key(s3FileName)).toString();
     }
 
-    // [private 메서드] DB에 업로드된 이미지를 전체 저장
-    private void createImage(List<String> urls, String uploadToken) {
-        List<Image> images = urls.stream()
+    /**
+     * [private 메서드]
+     * DB에 업로드된 이미지 저장
+     */
+    private void createImage(List<String> imageUrls, String uploadToken) {
+        // [Step 4-1] DB 저장을 위한 entity 객체생성
+        List<Image> images = imageUrls.stream()
                 .map(url -> Image.create(url, uploadToken, null))
                 .toList();
 
+        // [Step 4-2] 이미지 저장
         imageRepository.saveAll(images);
     }
 
